14cffe14c29287a75b4e9f369018b10b3a3210f5
Rafael Albuquerque <rafael.mor.alb@gmail.com>
2022-07-12 00:56:43 -0300

    add new test for the sample app

--- app/tests.py
+++ app/tests.py
@@ -1,9 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from app.models import SimpleModel
+
 
 class SampleTestCase(TestCase):
     def test_sum(self):
         x = 1
         y = 2
         self.assertEqual(x+y, 3)
+
+
+class SimpleModelTestCase(TestCase):
+    def test_model_creation(self):
+        SimpleModel.objects.create(name='test')
+        self.assertEqual(SimpleModel.objects.count(), 1)
+        simple_model = SimpleModel.objects.first()
+        self.assertEqual(simple_model.name, 'test')
